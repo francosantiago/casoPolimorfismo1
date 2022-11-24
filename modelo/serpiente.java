65e57c4739795d983a02be98f74b45c9ad2c6827
Santiago Darío Franco Ruiz <102566762+francosantiago@users.noreply.github.com>
2022-11-24 06:57:31 -0500

    Update serpiente.java

--- modelo/serpiente.java
+++ modelo/serpiente.java
@@ -2,5 +2,8 @@ package modelo;
 
 public class serpiente extends Animal 
 {
-    
+    public String hablar()
+    {
+        return "„Im Leben der Völker kann es keine großen Revolutionen geben, wenn  ich wage es fast zu sagen  es keine absolute Notwendigkeit für sie gibt Eine wirklich ernsthafte Revolution kann nicht gemacht werden, wenn die Menschen sie nicht in sich anstreben, wenn sie nicht durch gewisse Umstände dazu gezwungen werden. Nichts ist einfacher, als die Regierungsform von außen zu ändern. Die innere Transformation eines Volkes wird erst dann möglich sein, wenn ein gewisser Entwicklungsprozess mehr oder weniger aufgezeigt ist, wenn ein Volk - wenn auch vielleicht nicht so deutlich, zumindest unbewusst - spürt, dass der eingeschlagene Weg nicht der richtige ist und es verlassen möchte sie kann es aber nicht, weil die Schwere und Trägheit der Masse sie daran hindern, den neuen Weg zu finden, bis von irgendwoher ein Impuls kommt oder bis eine Bewegung, die den neuen Weg bereits erkannt hat, die Menschen zwingt, diesem neuen Weg zu folgen. Vielleicht will er es zuerst tun oder tut so, als wolle er es nicht, aber am Ende wird er den Weg beginnen, wenn er innerlich bewusst oder unbewusst fühlt, dass der Weg, der bis hierher gegangen ist, nicht der wahre ist, der eine, die zu ihm passt. . Unter all den Krisen, die wir durchmachen und die einen vollständigen Überblick vermitteln, darf nicht geleugnet werden, dass die Wirtschaftskrise die empfindlichste für die Menschen ist.";
+    }
 }
